d6bb8a6911b335ef17dd75b17a78a53fb2b61b48
yanxx297 <yanxx297@umn.edu>
2020-02-07 14:05:18 -0600

    Add verifiers (not yet complete)

--- c/verifier/verifier.c
+++ c/verifier/verifier.c
@@ -1,11 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void __VERIFIER_error() {
-    printf("Error Occurs!!!\n");
-    exit(1);
+// Errors
+void __VERIFIER_error() {}
+
+void __VERIFIER_error_linux_kernel_locking_rwlock(){}
+
+void __VERIFIER_error_linux_kernel_module(){}
+
+void __VERIFIER_error_linux_kernel_rcu_srcu(){}
+
+void __VERIFIER_error_linux_usb_gadget(){}
+
+// Inputs
+unsigned int __VERIFIER_nondet_uint() {
+    return 0;
 }
 
-unsigned int __VERIFIER_nondet_uint() {
+unsigned __VERIFIER_nondet_unsigned(){
+    return 0;
+}
+
+int __VERIFIER_nondet_int() {
+    return 0;
+}
+
+short __VERIFIER_nondet_short() {
+    return 0;
+}
+
+unsigned short __VERIFIER_nondet_ushort() {
+    return 0;
+}
+
+long __VERIFIER_nondet_long() {
+    return 0;
+}
+
+unsigned long __VERIFIER_nondet_ulong() {
+    return 0;
+}
+
+float __VERIFIER_nondet_float(){
+    return 0;
+}
+
+double __VERIFIER_nondet_double(){
+    return 0;
+}
+
+char __VERIFIER_nondet_char() {
+    return 0;
+}
+
+unsigned char __VERIFIER_nondet_uchar() {
+    return 0;
+}
+
+unsigned char __VERIFIER_nondet_unsigned_char(){
+    return 0;
+}
+
+_Bool __VERIFIER_nondet_bool() {
+    return 0;
+}
+
+long long __VERIFIER_nondet_longlong(){
+    return 0;
+}
+
+long long  __VERIFIER_nondet_loff_t() {
+    return 0;
+}
+
+size_t __VERIFIER_nondet_size_t(){
+    return 0;
+}
+
+void __VERIFIER_assume(int c){}
+
+void __VERIFIER_atomic_begin(){}
+
+void __VERIFIER_atomic_end(){}
+
+void * __VERIFIER_nondet_pointer(){
+    void *ptr = NULL;
+    return ptr;
+}
+
+unsigned short __VERIFIER_nondet_u16(){
+    return 0;
+}
+
+unsigned int __VERIFIER_nondet_u32(){
     return 0;
 }
